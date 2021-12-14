fb167c3fdb9c71d1f8d8e8fad85858f246118e67
PrecisionApps <73882757+PrecisionApps@users.noreply.github.com>
2021-12-14 22:16:04 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "Dictionary",
+    name: "Dictionary-KaanOzge",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "Dictionary",
+            name: "Dictionary-KaanOzge",
             targets: ["Dictionary"]),
     ],
     dependencies: [
